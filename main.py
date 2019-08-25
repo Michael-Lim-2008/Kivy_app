0c3a6a512509321ef739deb812321318aba736cc
Michael Lim <michaellim@MacBook-Pro.local>
2019-08-25 13:33:42 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+
+class WeatherApp(App):
+    pass
+
+
+if __name__ == "__main__":   # prevents files from being run if file is imported (only import code from kivy)
+    WeatherApp().run()    
